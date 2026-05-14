159fa0d63238c94dcbe146fb6d7d35d129d3b4f6
{ayan} <{ayandavian07@gmail.com}>
2026-05-15 00:57:29 +0530

    final

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://zealous-ocean-0f8a6d400.7.azurestaticapps.net"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
